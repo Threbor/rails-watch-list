f150cb0f31ffa8aa1c37c17f9e617b01523c0eda
Hugo ROBERT <hugo.pierre.robert@gmail.com>
2023-02-24 14:07:02 +0100

    dfghjk

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_one_attached :photo
   # A list must have a unique name.
   validates :name, presence: true, uniqueness: true
 
